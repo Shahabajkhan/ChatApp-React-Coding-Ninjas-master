fdd0ecabbcfdfd3503f65d3d374328cbfbedf0b0
Shahabajkhan Pathan <Pathanshahabaj2018@gmail.com>
2023-12-26 18:51:57 +0530

    first commit

---
+++ ChatApp-React-Coding-Ninjas-master/src/images/UserImage.js
@@ -0,0 +1,9 @@
+export default function user(state = {}) {
+  const imagePath = "C:/Users/91914/Downloads/ChatApp-React-Coding-Ninjas-master/ChatApp-React-Coding-Ninjas-master/src/images/background.jpg"; // Set your actual image path here
+
+  return {
+    id: 1,
+    name: "Shahabajkhan",
+    image: imagePath,
+  };
+}
---
+++ ChatApp-React-Coding-Ninjas-master/src/reducers/user.js
@@ -0,0 +1,22 @@
+// import imageSrc from 'ChatApp-React-Coding-Ninjas-master/src/images/InFrame_1694447168273.jpg';
+// export default function user(state = {}) {
+ 
+   
+//     return {
+//       id: 1,
+//       name: "Shahabajkhan",
+//       image: 
+//     }
+  
+// }
+
+
+export default function user(state = {}) {
+  const imagePath = "C:/Users/91914/Downloads/ChatApp-React-Coding-Ninjas-master/ChatApp-React-Coding-Ninjas-master/src/images/background.jpg"; // Set your actual image path here
+
+  return {
+    id: 1,
+    name: "Shahabajkhan",
+    image: imagePath,
+  };
+}
